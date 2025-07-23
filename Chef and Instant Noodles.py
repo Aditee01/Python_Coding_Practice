2ed7a00641018b20c399c0f563cc288cf1bc5313
Aditee Mohapatra <139526797+Aditee01@users.noreply.github.com>
2025-07-23 18:19:10 +0530

    Create Chef and Instant Noodles.py

---
+++ Chef and Instant Noodles.py
@@ -0,0 +1,9 @@
+# The first and only line of input contains two space-separated integers X and Y — the number of stoves and the number of minutes, respectively.
+
+# Sample -
+# Input: 3 7
+# Output: 21
+
+# cook your dish here
+x,y = map(int, input().split())
+print(x*y)
